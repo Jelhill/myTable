e20d9a749671f284e42420ce9e6ba1873b2cfb79
Jelhill <talk2jelili2000@gmail.com>
2020-03-12 11:51:00 +0100

    Updates

---
+++ precache-manifest.6456d22bf8c89a0dead3e374fe1ee51d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a47380a067b3d9880ad084991d5de13e",
+    "url": "/myTable/index.html"
+  },
+  {
+    "revision": "e2a4b6d7829add95f263",
+    "url": "/myTable/static/css/main.aeb462c6.chunk.css"
+  },
+  {
+    "revision": "bb42fc06715ba313518f",
+    "url": "/myTable/static/js/2.e7efe12f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/myTable/static/js/2.e7efe12f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2a4b6d7829add95f263",
+    "url": "/myTable/static/js/main.62504293.chunk.js"
+  },
+  {
+    "revision": "7c88cb1839dcf04e022f",
+    "url": "/myTable/static/js/runtime-main.2fb41784.js"
+  }
+]);
